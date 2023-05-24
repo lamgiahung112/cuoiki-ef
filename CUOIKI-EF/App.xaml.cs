7bc164e4817281fc654c4f1b60e0ad287f04c888
bananaback <58079308+bananaback@users.noreply.github.com>
2023-05-25 00:21:40 +0700

    change gitignore file

--- CUOIKI-EF/App.xaml.cs
+++ CUOIKI-EF/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using CUOIKI_EF.Enums;
-using CUOIKI_EF.Factories;
-using System.Windows;
+﻿using System.Windows;
 
 namespace CUOIKI_EF
 {
@@ -11,12 +9,6 @@ namespace CUOIKI_EF
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            using (var db = new DatabaseContext())
-            {
-                Employee currentEmp = EmployeeFactory.Create("Tin", "Tin", "Binh Dinh", EmployeeStatus.Active, "123", Gender.Male, Role.Dev);
-                db.Employees.Add(currentEmp);
-                db.SaveChanges();
-            }
             base.OnStartup(e);
         }
     }
